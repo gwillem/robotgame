08c32db923191cd0b1da3fc16c1c60c6bc757576
Willem de Groot <willem@byte.nl>
2013-11-19 17:33:48 +0100

    refactored moves to classes

--- bots/waterlinie.py
+++ bots/waterlinie.py
@@ -9,98 +9,104 @@ Essence of this bot:
 3. Execute plan for this.robot
 4. Profit!
 
-Todo:
-
-* Change proposal dict to ProposedMove
-* Change list to ProposedCollection
-
 BUGS
 
 * If many bots ATTACK the same location, that's fine! Should fix in 
   _reduce_list_of_dicts, it should require ALL arguments to satisfy 
   (dst=x && action=attack) 
 
+* If a bot is locked AND at spawn point AND can't flee but also can't attack, then it 
+  should run!
+
+* self.adjacents is ugly
 
 """
 
 from collections import Counter
-from itertools import chain
 import math
 import sys
 import time
 import rg
-from pprint import pprint
 
-def _prop_to_dict(prio, action, src, dst=None):
-    """ Proposal move to dictionary"""
+class ProposedMove(object):
     
-    if not dst:
-        dst = src
+    def __init__(self, prio, action, src, dst=None):
+        if not dst:
+            dst = src
+            
+        self.prio = prio
+        self.action = action
+        self.src = src
+        self.dst = dst
+        
+    def __str__(self):
+        return "%8s > %8s, %6s, (p:%3d)" % \
+            (self.src, self.dst, self.action, self.prio)
+        
+    def to_action(self):
+        action = [ self.action ]
+        if self.action in ['move','attack']:
+            action.append(self.dst)
+        return action
+        
+class ProposedMoveCollection(list):
     
-    return {
-        'prio': prio,
-        'action': action,
-        'src': src,
-        'dst': dst,
-    }
+    def _sort_proposals(self, p):
+        prio = 1 / p.prio if p.prio else 0
+        dist = rg.dist(p.dst, rg.CENTER_POINT)
+        angle = center_angle(p.dst)
     
-def _prop_to_action(prop):
-    if prop['action'] in ['guard','suicide']:
-        return [ prop['action'] ]
+        return (prio, dist, angle)
+       
+    def to_plan(self):
+        return dict((p.src, p.to_action()) for p in self)
+       
+    def find_singles(self):
+        """Return proposed moves for bots that have only a single proposal"""
+        sources = [p.src for p in self]
+        bots_with_single_prop = [ x[0] for x in Counter( sources ).items() if x[1] == 1]
+        
+        return [p for p in self if p.src in bots_with_single_prop]
+       
+    def add_move(self, *args):
+        self.append(ProposedMove(*args))
+       
+    def sort(self):
+        return super(ProposedMoveCollection, self).sort(key=self._sort_proposals)
+        
+    def __str__(self):
+        mystr = ""
+        for i, item in enumerate(self):
+            mystr += "%3d. %s\n" % (i, item)
+            
+        return mystr
+            
+        
+    def eliminate(self, **kwargs):
+        to_delete = []
     
-    return [ prop['action'], prop['dst'] ]
+        for i, item in enumerate(self):
+            ## do i need to delete this item?
+            for k, v in kwargs.items():
+                if getattr(item,k) == v:
+                    to_delete.append(i)
+                    break
 
-def _sort_proposals(prop):
-    """
-    Need deterministic sort, so use these keys:
-        1. priority
-        2. distance to center (for dst)
-        3. atan2 radius to center (for dst)
-    """
-    
-    prio = 1 / prop['prio'] if prop['prio'] else 0
-    dist = rg.dist(prop['dst'],rg.CENTER_POINT)
-    angle = _angle(prop['dst'])
-    
-    return (prio, dist, angle)
+        ## slit into separate code, because its tricky to manipulate a 
+        ## list and iterate it at the same time
+        for i in sorted(to_delete, reverse=True):
+            del self[i]
 
 def is_spawn(loc):
     return 'spawn' in rg.loc_types(loc)
 
-def _angle(loc, center=None):
+def center_angle(loc, center=None):
     if not center:
         center = rg.CENTER_POINT
     dx = loc[0] - center[0]
     dy = loc[1] - center[1]
     return math.atan2(dy, dx) 
-
-def _reduce_list_of_dicts(mylist, **kwargs):
-    """ Reduces list of dicts inplace by checking for key/value pairs in dicts as given by kwargs 
-    
-    >>> a = [
-            { 'x' : 1 },
-            { 'x' : 2 },
-            { 'x' : 3 },
-    ]
-
-    >>> _reduce_list(a, x=2)
-    >>> print a
-    [{'x': 1}, {'x': 3}]
-    """
-    to_delete = []
-    
-    for i, item in enumerate(mylist):
-        ## do i need to delete this item?
-        for k, v in kwargs.items():
-            if item[k] == v:
-                to_delete.append(i)
-                break
-
-    ## slit into separate code, because its tricky to manipulate a 
-    ## list and iterate it at the same time
-    for i in sorted(to_delete, reverse=True):
-        del mylist[i]
-                
+               
 class Robot():
         
     def act(self, game):
@@ -127,13 +133,11 @@ class Robot():
 
             proposals = self.collect_all_proposals()
             
-            print "proposals:"
-            #~ pprint(proposals)
+            print "proposals:\n%s" % proposals
             
             plan = self.proposals_to_plan(proposals)
         
-            print "plan:"
-            #~ pprint(plan)
+            print "plan: %s" % plan
             
             self.history_plan[self.turn] = plan
             
@@ -145,7 +149,7 @@ class Robot():
 
         if self.location not in plan:
             print "Ouch! Fatal error! I couldn't find myself %s in the plan" % (self.location,)
-            raise Exception
+            raise Exception("My plan calculation is flawed, as I couldn't find myself")
         
         return plan[self.location]
         
@@ -170,24 +174,20 @@ class Robot():
         
         return locs
 
+    def find_all_bots(self, player_id=None):
+        
+        if player_id:
+            return [loc for loc in self.robots if self.robots[loc]['player_id'] == player_id]
+        else:
+            return [loc for loc in self.robots]    
+        
+
     def collect_all_proposals(self):
-        """ Calculate proposed moves for all of my peers 
+        """ Calculate proposed moves for all of my peers """
         
-        Output: proposals = [
-            { 
-                'prio': 100,
-                'action': 'move',
-                'src': (x,y),
-                'dst': (x,y)
-                
-            }
-        ]
-        """
-        peers = [loc for loc in self.robots if self.player_id == self.robots[loc]['player_id']]
-        #~ print "I found %d peers" % len(peers)
+        proposals = ProposedMoveCollection()
         
-        proposals = []
-        for peer in peers:
+        for peer in self.find_all_bots(self.player_id):
             proposals.extend(self.calculate_proposals_for_loc(peer))
         
         return proposals
@@ -195,17 +195,22 @@ class Robot():
     
     def try_to_flee(self, src):
         locs = self.adjacents(src, only_empty=True)
-        # find locs with least amound of enemy neighbours
+        
+        # find locs with least amount of enemy neighbours
         locs = [loc for loc in locs if self.count_enemy_neighbours_for_loc(loc=loc) == 0]
         
         if locs:
-            return _prop_to_dict(100, 'move', src, locs[0])
+            return ProposedMove(100, 'move', src, locs[0])
         
-        raise RuntimeError("Can't flee! No safe locations")
+        raise CannotFlee("Can't flee! No safe locations")
         
     def attack_weakest_neighbour(self, src):
-        enemies = self.enemy_neighbours_for_loc() # already sorted on HP
-        return _prop_to_dict(100, 'attack', src, enemies[0])
+        enemies = self.enemy_neighbours_for_loc(loc=src) # already sorted on HP
+        
+        if not enemies:
+            raise NoBotFound
+            
+        return ProposedMove(100, 'attack', src, enemies[0])
     
     def find_preemptive_strike(self, src):
         ## which neighbour has most enemy neighbours?
@@ -213,19 +218,19 @@ class Robot():
         neighbours = [(n,self.count_enemy_neighbours_for_loc(loc=n)) for n in neighbours]
         neighbours.sort(key = lambda x: x[1], reverse=True)
         
-        print "%s : %s" % (src, neighbours)
+        #~ print "%s : %s" % (src, neighbours)
         
         if neighbours and neighbours[0][1]: ## does the no 1 have enemy neighbours?
-            return _prop_to_dict(100, 'attack', src, neighbours[0][0]) 
+            return ProposedMove(100, 'attack', src, neighbours[0][0]) 
         else:
             return None
     
     def calculate_proposals_for_loc(self, src):
         ## find possible moves
-        proposals = []
+        proposals = ProposedMoveCollection()
 
         ## stand still is also valid
-        proposals.append(_prop_to_dict(50, 'guard', src))
+        proposals.add_move(50, 'guard', src)
         
         now_surrounded = self.count_enemy_neighbours_for_loc(loc=src)
         preemptive_strike = self.find_preemptive_strike(src)
@@ -252,7 +257,7 @@ class Robot():
             try:
                 #~ print "%s : Trying to flee" % (src,)
                 proposals.append(self.try_to_flee(src))
-            except RuntimeError:
+            except CannotFlee:
                 #~ print "%s : Can't flee, will attack weakest neighbour" % (src,)
                 proposals.append(self.attack_weakest_neighbour(src))
                 
@@ -264,11 +269,16 @@ class Robot():
             possibles = self.adjacents(src, only_empty=True)
             
             src_peer_neighbours = self.count_peer_neighbours_for_loc(src)
+            src_center_distance = rg.dist(src, rg.CENTER_POINT)
+            
             
             for dst in possibles:
                 score = 49
                 
                 dst_peer_neighbours = self.count_peer_neighbours_for_loc(dst) - 1 
+                dst_center_distance = rg.dist(dst, rg.CENTER_POINT)
+                
+                
                 
                 #~ print "%s has %d peer neighbours" % (dst, dst_peer_neighbours)
                 
@@ -280,41 +290,15 @@ class Robot():
                     score += 5
                 
                 if dst_peer_neighbours < src_peer_neighbours:
-                    score += (9-dst_peer_neighbours) ## dont exceed 100
+                    score += 2 * (9-dst_peer_neighbours) ## dont exceed 100
+                    
+                if dst_center_distance < src_center_distance:
+                    score += 2
+                
+                if dst_center_distance > src_center_distance:
+                    score -= 1
 
-                prop = _prop_to_dict(score, 'move', src, dst)
-                proposals.append(prop)
-        
-        ## TODO, continue here
-        
-        #~ possibles = self.adjacents(src)        
-        #~ for dst in possibles:
-            #~ 
-            #~ ## 1. Do I have enemy neighbour and can I step back?
-            #~ 
-            #~ ## 2. 
-            #~ 
-            #~ score = 50
-            #~ 
-            #~ src_dist = rg.dist(src, rg.CENTER_POINT)
-            #~ dst_dist = rg.dist(dst, rg.CENTER_POINT)
-            #~ 
-            #~ dst_surrounded = self.count_enemy_neighbours_for_loc(loc=dst)
-            #~ 
-            #~ if dst_surrounded < now_surrounded:
-                #~ score += 10 * (10-dst_surrounded)
-            #~ elif dst_surrounded > now_surrounded:
-                #~ score -= 30
-            #~ 
-            #~ if dst_dist < src_dist:
-                #~ score += 10
-            #~ elif dst_dist == src_dist:
-                #~ score -= 1
-            #~ else:
-                #~ score -= 5
-            #~ 
-            #~ prop = _prop_to_dict(score, 'move', src, dst)
-            #~ proposals.append(prop)
+                proposals.add_move(score, 'move', src, dst)
         
         return proposals
         
@@ -330,60 +314,34 @@ class Robot():
             a. Check if either source or destination hasn't been processed already (yes > skip)
             b. add to moves[] and new/old_field
         
-        Input: proposals = [
-            { 
-                'prio': 100,
-                'action': 'move',
-                'src': (x,y),
-                'dst': (x,y)
-                
-            }
-        ]
+        Input: proposals = ProposedMoveCollection
         
         Output: plan = {
             (x,y): ['guard'],
             (p,q): ['attack', (y,z)],
-        
         }
         """
 
-        proposals.sort(key=_sort_proposals)
-        moves = []
+        proposals.sort()
+        moves = ProposedMoveCollection()
         
         while proposals:
             
-            #~ print "i have %d props" % len(proposals)
-            #~ pprint(proposals)
-
             ## 1. check if there are bots with one proposal only
-            sources = [p['src'] for p in proposals]
-            single_src = [ x[0] for x in Counter( sources ).items() if x[1] == 1]
-            singles = [p for p in proposals if p['src'] in single_src]
-            #~ print "I found %d singles (%s)" % (len(singles), singles)
-                        
-            if singles:
-                to_execute = singles
-            else:
-                to_execute = [ proposals.pop(0) ]
+            execute_proposals = proposals.find_singles()
+            
+            if not execute_proposals: ## if not, then just pick highest prio
+                execute_proposals = [ proposals.pop(0) ]
                 
-            for prop in to_execute:
-
-                #~ print "will execute prop %s" % prop
-                ## remove bots with these src/dst from the proposals list
-                _reduce_list_of_dicts(proposals, src=prop['src'])
-                _reduce_list_of_dicts(proposals, dst=prop['dst'])
+            for p in execute_proposals:
+                proposals.eliminate(src=p.src)
+                proposals.eliminate(dst=p.dst)
 
                 ## maintain master list of final moves
-                moves.append(prop)
+                moves.append(p)
 
-        # here, i have no more items in my proposal list, and everything converted to 
-        # a list of unique moves
-        
         # moves is a list of proposals, need to transform to api-compatible format
-        
-        plan = dict((p['src'],_prop_to_action(p)) for p in moves)
-        
-        return plan
+        return moves.to_plan()
 
     def count_enemy_neighbours_for_loc(self, **kwargs):
         return len(self.enemy_neighbours_for_loc(**kwargs))
@@ -413,40 +371,23 @@ class Robot():
         
 class TestRobot(object):
     
-    def test_reduce_list_of_dicts(self):
+    def test_proposed_move_collection(self):
         
-        a = [{'x':2},{'x':2},{'x':2}]
-        _reduce_list_of_dicts(a, x=2)
-        assert len(a) == 0, a
+        pmc = ProposedMoveCollection()
+        
+        for i in range(3):
+            pmc.add_move(100, 'move', (1,1), (i,i))
+        assert len(pmc) == 3, pmc
+        
+        pmc.eliminate(dst=(1,1))
+        assert len(pmc) == 2, pmc
+        
+        pmc.eliminate(action='move')
+        assert len(pmc) == 0, pmc
 
-        a = [{'action': 'move', 'dst': (5, 15), 'prio': 60, 'src': (5, 16)},
-             {'action': 'move', 'dst': (6, 16), 'prio': 60, 'src': (5, 16)},
-             {'action': 'guard', 'dst': (5, 16), 'prio': 30, 'src': (5, 16)}]
-             
-        _reduce_list_of_dicts(a, dst=(5,15))
-        assert len(a) == 2, a
-             
-        _reduce_list_of_dicts(a, src=(5,16))
-        assert len(a) == 0, a
-             
-        a = [
-            { 'x' : 1 },
-            { 'x' : 2 },
-            { 'x' : 3 },
-        ]
         
-        _reduce_list_of_dicts(a, x=2)
-        assert a == [{'x': 1}, {'x': 3}]
-        
-        
-    def test_preemptive_strike(self):
-        robot = Robot()
-        robot.robots = {
-                (5,5): { 'location': (5, 5), 'hp': 50, 'player_id': 0,  },
-                (5,7): { 'location': (5, 7), 'hp': 50, 'player_id': 1,  },
-        }
-        robot.player_id = 0
-        robot.hp = 50
-        
-        print robot.find_preemptive_strike((5,5))
-        
+class NoBotFound(Exception):
+    pass
+
+class CannotFlee(Exception):
+    pass
